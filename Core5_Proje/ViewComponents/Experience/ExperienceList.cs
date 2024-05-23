a1f9f69582d296f716e98803e8732e8e3b75a478
shirinze <sherize1373@gmail.com>
2024-05-23 15:00:56 +0300

    added Experience List with viewcomponent

---
+++ Core5_Proje/ViewComponents/Experience/ExperienceList.cs
@@ -0,0 +1,15 @@
+﻿using BuinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core5_Proje.ViewComponents.Experience
+{
+    public class ExperienceList:ViewComponent
+    {
+        ExperienceManager experiencemanager = new ExperienceManager(new EfExperienceDal());
+        public IViewComponentResult Invoke()
+        {
+            return View(experiencemanager.TGetByList());
+        }
+    }
+}
