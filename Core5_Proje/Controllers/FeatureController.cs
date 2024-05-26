f4d070110d45aecbf261179ba0e0931769b51873
shirinze <sherize1373@gmail.com>
2024-05-26 18:23:55 +0300

    deleted template

---
+++ Core5_Proje/Controllers/FeatureController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core5_Proje.Controllers
+{
+    public class FeatureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
