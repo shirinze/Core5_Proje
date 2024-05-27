f92097d7525bd90a3d96cd39706426d8200f6b2e
shirinze <sherize1373@gmail.com>
2024-05-27 11:49:19 +0300

    added new admin template

---
+++ Core5_Proje/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core5_Proje.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+    }
+}
