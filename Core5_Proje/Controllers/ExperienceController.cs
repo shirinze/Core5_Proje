6a10f34f31409044a245b865275d8b8347af2a4b
shirinze <sherize1373@gmail.com>
2024-05-28 11:44:39 +0300

    added experience controller

---
+++ Core5_Proje/Controllers/ExperienceController.cs
@@ -0,0 +1,15 @@
+﻿using BuinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core5_Proje.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceManager experiencemanager = new ExperienceManager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+            return View(experiencemanager.TGetByList());
+        }
+    }
+}
--- Core5_Proje/Controllers/SkillController.cs
+++ Core5_Proje/Controllers/SkillController.cs
@@ -2,6 +2,7 @@
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concreate;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.Intrinsics;
 
 namespace Core5_Proje.Controllers
 {
@@ -34,6 +35,21 @@ namespace Core5_Proje.Controllers
             var value = skillmanager.TGetById(id);
             skillmanager.TDelete(value);
             return RedirectToAction("Index");
+        }
+
+        public IActionResult SkillUpdate(int id)
+        {
+            ViewBag.v1 = "Skill";
+            ViewBag.v2 = "skillupdate";
+            ViewBag.v3 = "Skill";
+
+            return View(skillmanager.TGetById(id));
+        }
+        [HttpPost]
+        public IActionResult SkillUpdate(Skill s)
+        {
+            skillmanager.TUpdate(s);
+            return RedirectToAction("Index","Skill");
         }
 
 
