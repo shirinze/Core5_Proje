4cbe97caa946af95d9d98af01c82ff6231519ef0
shirinze <sherize1373@gmail.com>
2024-05-28 13:51:04 +0300

    added Experience controler

--- Core5_Proje/Controllers/ExperienceController.cs
+++ Core5_Proje/Controllers/ExperienceController.cs
@@ -1,5 +1,6 @@
 ﻿using BuinessLayer.Concreate;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concreate;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core5_Proje.Controllers
@@ -9,7 +10,43 @@ namespace Core5_Proje.Controllers
         ExperienceManager experiencemanager = new ExperienceManager(new EfExperienceDal());
         public IActionResult Index()
         {
+            ViewBag.v1 = "Experience";
+            ViewBag.v2 = "list";
+            ViewBag.v3 = "Experience";
             return View(experiencemanager.TGetByList());
+        }
+        public IActionResult ExperienceAdd()
+        {
+            ViewBag.v1 = "Experience";
+            ViewBag.v2 = "Add";
+            ViewBag.v3 = "Experience";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ExperienceAdd(Experience experience)
+        {
+            experiencemanager.TAdd(experience);
+            return RedirectToAction("Index","Experience");
+        }
+
+        public IActionResult ExperienceDelete(int id)
+        {
+            var value = experiencemanager.TGetById(id);
+            experiencemanager.TDelete(value);
+            return RedirectToAction("Index","Experience");
+        }
+        public IActionResult ExperienceUpdate(int id)
+        {
+            ViewBag.v1 = "Experience";
+            ViewBag.v2 = "Update";
+            ViewBag.v3 = "Experience";
+            return View(experiencemanager.TGetById(id));
+        }
+        [HttpPost]
+        public IActionResult ExperienceUpdate(Experience experience)
+        {
+            experiencemanager.TUpdate(experience);
+            return RedirectToAction("Index", "Experience");
         }
     }
 }
