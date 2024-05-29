36da3a4b8bad484be3a225fa47c8f3137d35a24a
shirinze <sherize1373@gmail.com>
2024-05-29 13:35:44 +0300

    added portfolio controller

---
+++ Core5_Proje/Controllers/PortfolioController.cs
@@ -0,0 +1,33 @@
+﻿using BuinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concreate;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core5_Proje.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioManager portfoliomanager = new PortfolioManager(new EfPortfolioDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Portfolio";
+            ViewBag.v2 = "PortfolioList";
+            ViewBag.v3 = "Portfolio";
+            return View(portfoliomanager.TGetByList());
+        }
+
+        public IActionResult AddPortfolio()
+        {
+            ViewBag.v1 = "Portfolio";
+            ViewBag.v2 = "PortfoliAdd";
+            ViewBag.v3 = "Portfolio";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPortfolio(Portfolio p)
+        {
+            portfoliomanager.TAdd(p);
+            return RedirectToAction("Index", "Portfolio");
+        }
+    }
+}
