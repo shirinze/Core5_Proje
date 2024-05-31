e1c9d091c3f2e366e5382d831d6944c2e25b30be
shirinze <sherize1373@gmail.com>
2024-05-31 14:32:39 +0300

    added dashboard

---
+++ Core5_Proje/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core5_Proje.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Dashboard";
+            ViewBag.v2 = "Dashboard";
+            ViewBag.v3 = "Dashboard Pannel";
+            return View();
+        }
+    }
+}
---
+++ Core5_Proje/ViewComponents/Dashboard/Featurestatistics.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core5_Proje.ViewComponents.Dashboard
+{
+    public class Featurestatistics:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
