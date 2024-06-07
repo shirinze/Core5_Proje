9ec018d668cf3713b89597b22af6517693d67780
shirinze <sherize1373@gmail.com>
2024-06-07 12:00:45 +0300

    dasshboard revizeleri

--- Core5_Proje/ViewComponents/Dashboard/Featurestatistics.cs
+++ Core5_Proje/ViewComponents/Dashboard/Featurestatistics.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concreate;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Core5_Proje.ViewComponents.Dashboard
 {
     public class Featurestatistics:ViewComponent
     {
+        Context c = new Context();
         public IViewComponentResult Invoke()
         {
+            ViewBag.v1 = c.Skills.Count();
+            ViewBag.v2 = c.Messages.Where(x => x.Status == false).Count();
+            ViewBag.v3 = c.Messages.Where(x => x.Status == true).Count();
+            ViewBag.v4 = c.Experiences.Count();
+
             return View();
         }
     }
