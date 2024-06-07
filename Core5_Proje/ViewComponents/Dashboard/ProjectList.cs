8d6c2ebd900eb0db6fbb932bab6e583bd2da0cc6
shirinze <sherize1373@gmail.com>
2024-06-07 16:00:37 +0300

    dashboard

---
+++ Core5_Proje/ViewComponents/Dashboard/ProjectList.cs
@@ -0,0 +1,15 @@
+﻿using BuinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core5_Proje.ViewComponents.Dashboard
+{
+    public class ProjectList:ViewComponent
+    {
+        PortfolioManager portfoliomanager = new PortfolioManager(new EfPortfolioDal());
+        public IViewComponentResult Invoke()
+        {
+            return View(portfoliomanager.TGetByList());
+        }
+    }
+}
---
+++ Core5_Proje/ViewComponents/Dashboard/StatisticDashboard2.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concreate;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Core5_Proje.ViewComponents.Dashboard
+{
+    public class StatisticDashboard2:ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = c.Portfolios.Count();
+            ViewBag.v2 = c.Messages.Count();
+            ViewBag.v3 = c.Services.Count();
+            return View();
+        }
+    }
+}
