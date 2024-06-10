f5e57c76b1626c746489cdba6b9f59e89b5f0a5c
shirinze <sherize1373@gmail.com>
2024-06-10 12:24:17 +0300

    added slide list

---
+++ Core5_Proje/ViewComponents/Portfolio/SlideList.cs
@@ -0,0 +1,17 @@
+﻿using BuinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core5_Proje.ViewComponents.Portfolio
+{
+    public class SlideList:ViewComponent
+    {
+        PortfolioManager portfoliomanager = new PortfolioManager(new EfPortfolioDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = portfoliomanager.TGetByList();
+            return View(values);
+        }
+    }
+}
+
