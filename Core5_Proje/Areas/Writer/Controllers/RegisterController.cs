5e293f51296270c9a20f9ccf291a0ba50d3cb5f8
shirinze <sherize1373@gmail.com>
2024-06-12 13:47:40 +0300

    added userregister viewmodel

---
+++ Core5_Proje/Areas/Writer/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using Core5_Proje.Areas.Writer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core5_Proje.Areas.Writer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [Area("Writer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(UserRegisterViewModel userviewmodel)
+        {
+            if(ModelState.IsValid)
+            {
+                
+            }
+            return View();
+        }
+    }
+}
---
+++ Core5_Proje/Areas/Writer/Models/UserRegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core5_Proje.Areas.Writer.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage ="please enter the username.")]
+        public string UserName { get; set; } 
+
+        [Required(ErrorMessage ="please enter the password.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage ="please enter the password again.")]
+        [Compare("Password",ErrorMessage ="password not the same.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage ="please enter the email")]
+        public string Mail { get; set; }
+    }
+}
