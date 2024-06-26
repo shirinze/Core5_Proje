2eb02989439526af990909db5b51e13c964b5437
shirinze <sherize1373@gmail.com>
2024-06-26 12:00:12 +0300

    profile added

--- Core5_Proje/Areas/Writer/Controllers/ProfileController.cs
+++ Core5_Proje/Areas/Writer/Controllers/ProfileController.cs
@@ -1,6 +1,9 @@
-﻿using EntityLayer.Concreate;
+﻿using Core5_Proje.Areas.Writer.Models;
+using EntityLayer.Concreate;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Core5_Proje.Areas.Writer.Controllers
@@ -13,10 +16,40 @@ namespace Core5_Proje.Areas.Writer.Controllers
         {
             _usermanager = usermanager;
         }
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var values = await _usermanager.FindByNameAsync(User.Identity.Name);
-            return View(values);
+            UserEditeViewModel model = new UserEditeViewModel();
+            model.Name = values.Name;
+            model.Surename = values.SureName;
+            model.PictureUrl=values.ImageUrl;
+            return View(model);
         }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditeViewModel p)
+        {
+            //resim yukleme icin 
+            var user = await _usermanager.FindByNameAsync(User.Identity.Name);
+            if(p.Picture!=null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(p.Picture.FileName);
+                var imagename = Guid.NewGuid() + extension;
+                var savelocation = resource + "/wwwroot/Userimage/" + imagename;
+                var strem = new FileStream(savelocation, FileMode.Create);
+                await p.Picture.CopyToAsync(strem);
+                user.ImageUrl = imagename;
+            }
+            user.Name = p.Name;
+            user.SureName = p.Surename;
+            var result = await _usermanager.UpdateAsync(user);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
+        }
+      
     }
 }
---
+++ Core5_Proje/Areas/Writer/Models/UserEditeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Core5_Proje.Areas.Writer.Models
+{
+    public class UserEditeViewModel
+    {
+        public string Name { get; set; }
+        public string Surename { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+        public string PictureUrl { get; set; }
+        public IFormFile Picture { get; set; } //root ıcınde kaydetmek ıcın
+    }
+}
