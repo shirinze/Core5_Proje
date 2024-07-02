44823ad10b3134e52c4292425cbaa8f7778dea13
shirinze <sherize1373@gmail.com>
2024-07-02 13:22:50 +0300

    navbar and notification added to dashboard

---
+++ Core5_Proje/Areas/Writer/ViewComponents/Navbar.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concreate;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Core5_Proje.Areas.Writer.ViewComponents
+{
+    public class Navbar:ViewComponent
+    {
+        private readonly UserManager<WriterUser> _usermanger;
+        public Navbar(UserManager<WriterUser> usermanger)
+        {
+            _usermanger = usermanger;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _usermanger.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = values.ImageUrl;
+            return View();
+        }
+    }
+}
--- Core5_Proje/Areas/Writer/ViewComponents/Notification.cs
+++ Core5_Proje/Areas/Writer/ViewComponents/Notification.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BuinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Core5_Proje.Areas.Writer.ViewComponents
 {
     public class Notification:ViewComponent
     {
+        AnnouncementManager announcementmanager = new AnnouncementManager(new EfAnnounementDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            return View(announcementmanager.TGetByList());
         }
     }
 }
