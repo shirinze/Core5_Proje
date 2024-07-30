7a9150261d60899b12a04b0daaf625471950bbaa
shirinze <sherize1373@gmail.com>
2024-07-30 14:22:43 +0300

    socialmedia complated

---
+++ Core5_Proje/ViewComponents/SocialMedia/SocialMediaList.cs
@@ -0,0 +1,16 @@
+﻿using BuinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core5_Proje.ViewComponents.SocialMedia
+{
+    public class SocialMediaList:ViewComponent
+    {
+        SocialmediaManager socialmedia = new SocialmediaManager(new EfSocialMediaDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = socialmedia.TGetByList();
+            return View(values);
+        }
+    }
+}
