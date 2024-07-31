82b71397ebbaa04ce8d913705bcf9b96e10aa072
shirinze <sherize1373@gmail.com>
2024-07-31 10:55:23 +0300

    dashboard

--- Core5_Proje/Controllers/DashboardController.cs
+++ Core5_Proje/Controllers/DashboardController.cs
@@ -6,9 +6,6 @@ namespace Core5_Proje.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.v1 = "Dashboard";
-            ViewBag.v2 = "Dashboard";
-            ViewBag.v3 = "Dashboard Pannel";
             return View();
         }
     }
---
+++ Core5_Proje/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -0,0 +1,18 @@
+﻿using BuinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Core5_Proje.ViewComponents.Dashboard
+{
+    public class AdminNavbarMessageList:ViewComponent
+    {
+        WriterMessageManager writermessagemanager = new WriterMessageManager(new EfWriterMessageDal());
+        public IViewComponentResult Invoke()
+        {
+            string p = "Admin@gmail.com";
+            var values = writermessagemanager.GetListRecieverMessage(p).OrderByDescending(x => x.WriterMessageId).Take(3).ToList();
+            return View(values);
+        }
+    }
+}
